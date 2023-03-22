7f274b5e513d4c2f62260180870195a50b939af8
DAAN <daanmgneni@gmail.com>
2023-03-22 14:14:26 +0530

    Added Main Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using EmpWagev3;
+
+namespace OOPS
+{
+    class EmpWage
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master");
+            
+        }
+    }
+}
