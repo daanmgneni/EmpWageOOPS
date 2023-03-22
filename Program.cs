35ead2866931d8169eaaae974fe19834a1cc4eb9
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-03-22 16:02:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace OOPS
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master");
+             CheckEmpAttendance obj= new CheckEmpAttendance();
+            obj.EmpCheck();
             
         }
     }
