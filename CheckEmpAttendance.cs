7da8d1ae4910d805d71d56a35aa9277e8af313b3
DAAN <daanmgneni@gmail.com>
2023-03-22 14:22:36 +0530

    [Dhanesh]:Added program to check employ attendance

---
+++ CheckEmpAttendance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagev3
+{
+    public class CheckEmpAttendance
+    {
+        
+
+        public void EmpCheck()
+        {
+            int Ispresent = 1;
+            Random rnd = new Random();
+            int Attendance=rnd.Next();
+
+            if ( Attendance == Ispresent )
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+
+        
+        
+
+        
+     
+
+
+    }
+}
