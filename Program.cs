13a82f3dd569a0c0561a5cfb3d8965d962660493
DAAN <daanmgneni@gmail.com>
2023-03-25 08:30:00 +0530

    program for manage multiple compony using array list

--- Program.cs
+++ Program.cs
@@ -6,30 +6,33 @@ namespace EmpWageComputationProblemWithOOP
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the employee wage problems");
-            CompanyEmpWage[] Company = new CompanyEmpWage[5];
+            List<ParentComputeEmpWage> companies = new List<ParentComputeEmpWage>();
 
             Console.WriteLine("Welcome to Employee Wage Computation Program On Master Branch");
             Console.WriteLine("----------------------------------------------------------------------");
 
-            for (int i = 0; i < Company.Length; i++)
+            while (true)
             {
                 Console.WriteLine("******************:MENU:******************");
                 Console.WriteLine("PRESS 1: To Calculate the wage in a Company.");
                 Console.WriteLine("PRESS 2: To EXIT.");
+
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 1)
                 {
                     Console.WriteLine("Enter the details:");
-                    Company[i] = new CompanyEmpWage();
+                    companies.Add(new CompanyEmpWage());
                     Console.WriteLine("_________________________________________________");
-                    Company[i].EmpWageBuilder();
+                    companies[companies.Count - 1].EmpWageBuilder();
                 }
-                else
+                else if (choice == 2)
                 {
-                    i = Company.Length;
+                    break;
                 }
             }
+
+
+
         }
     }
 }
